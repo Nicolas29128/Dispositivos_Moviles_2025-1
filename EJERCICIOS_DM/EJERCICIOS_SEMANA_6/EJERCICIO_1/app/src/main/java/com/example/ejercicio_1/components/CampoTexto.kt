98ac30ab8e93229dce09fc636cfeafa37258c16f
Nicolas29128 <nr4974727@gmail.com>
2026-05-03 16:39:10 -0500

    creacion del campo texto
    en esta parte solo esta lo que seria la validación y muestra lo que seria el mensaje de error.

---
+++ EJERCICIOS_DM/EJERCICIOS_SEMANA_6/EJERCICIO_1/app/src/main/java/com/example/ejercicio_1/components/CampoTexto.kt
@@ -0,0 +1,37 @@
+package com.example.ejercicio_1.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CampoTexto (
+    valor : String,
+    label : String,
+    esError : Boolean,
+    mensajeError : String,
+    onValueChange : (String) -> Unit
+){
+    OutlinedTextField(
+        value = valor,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        isError = esError,
+        modifier = Modifier.fillMaxWidth()
+    )
+    if (esError) {
+        Text(
+            text = mensajeError,
+            color = MaterialTheme.colorScheme.error,
+            style = MaterialTheme.typography.titleSmall
+        )
+    }
+
+    Spacer(modifier = Modifier.height(20.dp))
+}
